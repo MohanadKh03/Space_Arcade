242df5530a6f5fb7d35c01a4278ac72c43cc689f
MohamedGamalBarghash <cinosmoh@gmail.com>
2022-04-01 16:27:20 +0200

    321

--- Source.cpp
+++ Source.cpp
@@ -44,7 +44,7 @@ int main()
     camera.setCenter(YP.getPosition());
     YP.setTexture(t);
     YP.setTextureRect(IntRect(x * 64, 0 * 64, 64, 64));
-    YP.setPosition(window, 0);                                                                        //window AND photo
+    YP.setPosition(500, 500);                                                                        //window AND photo
     //ggggggggggg
     while (window.isOpen())
     {
