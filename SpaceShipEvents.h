20838e23edb6cfa2b9b199247283f5da95710ceb
MohamedGamalBarghash <cinosmoh@gmail.com>
2022-04-06 02:59:13 +0200

    Removed the useless boxes

--- SpaceShipEvents.h
+++ SpaceShipEvents.h
@@ -9,34 +9,9 @@ void MovementSpaceShip(RenderWindow& window, View& camera,Sprite& YP,int& x,int&
 	const float GMscale = 0.15;
 	GM1.setScale(GMscale, GMscale);
 	GM1.setPosition(0, 0);
-	Texture box4;
-	box4.loadFromFile("box3.png");
-	Sprite box5(box4);
-	const float box4scale = 1.2;
-	box5.setScale(box4scale, box4scale);
-	box5.setPosition(0, 0);
-	Texture box2;
-	box2.loadFromFile("box3.png");
-	Sprite box3(box2);
-	const float box2scale = 1.2;
-	box3.setScale(box2scale, box2scale);
-	box3.setPosition(0, 0);
-	Texture box;
-	box.loadFromFile("boxs2.png");
-	Sprite box1(box);
-	const float boxscale = 1.2;
-	box1.setScale(boxscale, boxscale);
-	box1.setPosition(0, 0);
-	Texture box6;
-	box6.loadFromFile("boxs2.png");
-	Sprite box7(box6);
-	const float box6scale = 1.2;
-	box7.setScale(box6scale, box6scale);
-	box7.setPosition(0, 0);
-	int screenXRightBorders = 20;
-	int screenXLeftBorders = 20;
-	int screenYTopBorders = 100;
-	int screenYBotBorders = 200;
+
+	int screenXBorders = 20;
+	int screenYBorders = 100;
 
 	if (Keyboard::isKeyPressed(Keyboard::Right) && YP.getPosition().x < window.getSize().x - screenXBorders)
 	{
@@ -56,7 +31,7 @@ void MovementSpaceShip(RenderWindow& window, View& camera,Sprite& YP,int& x,int&
 		y = 1;
 		YP.setTextureRect(IntRect(x * 64, y * 64, 64, 64));
 	}
-	else if (Keyboard::isKeyPressed(Keyboard::Up) && YP.getPosition().y > screenYTopBorders)
+	else if (Keyboard::isKeyPressed(Keyboard::Up) && YP.getPosition().y > screenYBorders)
 	{
 		YP.move(0, -10);
 		camera.move(0, -10);
@@ -65,7 +40,7 @@ void MovementSpaceShip(RenderWindow& window, View& camera,Sprite& YP,int& x,int&
 		y = 3;
 		YP.setTextureRect(IntRect(x * 64, y * 64, 64, 64));
 	}
-	else if (Keyboard::isKeyPressed(Keyboard::Down) && YP.getPosition().y < window.getSize().y - screenYBotBorders)
+	else if (Keyboard::isKeyPressed(Keyboard::Down) && YP.getPosition().y < window.getSize().y - screenYBorders)
 	{
 		YP.move(0, 10);
 		camera.move(0, 10);
@@ -75,33 +50,5 @@ void MovementSpaceShip(RenderWindow& window, View& camera,Sprite& YP,int& x,int&
 		YP.setTextureRect(IntRect(x * 64, y * 64, 64, 64));
 
 	}
-	
-	if (box1.getGlobalBounds().intersects(YP.getGlobalBounds()) || box3.getGlobalBounds().intersects(YP.getGlobalBounds()) || box5.getGlobalBounds().intersects(YP.getGlobalBounds()) || box7.getGlobalBounds().intersects(YP.getGlobalBounds()) || GM1.getGlobalBounds().intersects(YP.getGlobalBounds()))
-	{
-		if (Keyboard::isKeyPressed(Keyboard::Down))
-		{
-			YP.move(0,-0.1);
-			camera.move(0, -0.1);
-		}
-		else if (Keyboard::isKeyPressed(Keyboard::Up))
-		{
-			YP.move(0, 0.1);
-			camera.move(0, 0.1);
-		}
-		else if (Keyboard::isKeyPressed(Keyboard::Left))
-		{
-			YP.move(0.1, 0);
-			camera.move(0.1, 0);
-		}
-		else if (Keyboard::isKeyPressed(Keyboard::Right))
-		{
-			YP.move(-0.1, 0);
-			camera.move(-0.1, 0);
-		}
-	}
 	window.draw(GM1);
-	window.draw(box1);
-	window.draw(box3);
-	window.draw(box5);
-	window.draw(box7);
 }
