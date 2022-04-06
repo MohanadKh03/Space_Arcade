4eb07d47d15d652f0f4c777af5368307c923fd0a
MohamedGamalBarghash <cinosmoh@gmail.com>
2022-04-06 02:56:46 +0200

    Added New Background

--- Source.cpp
+++ Source.cpp
@@ -23,48 +23,17 @@ int main()
                                                                              //window AND photo
     
     int score_BrickBreaker = 0;
-    // gg
-    Texture GM;
-    GM.loadFromFile("game machine.png");
-    Sprite GM1(GM);
-    const float GMscale = 0.15;
-    GM1.setScale(GMscale, GMscale);
-    GM1.setPosition(-480, -250);
-    Texture box4;
-    box4.loadFromFile("box3.png");
-    Sprite box5(box4);
-    const float box4scale = 1.2;
-    box5.setScale(box4scale, box4scale);
-    box5.setPosition(-360, 80);
-    Texture box2;
-    box2.loadFromFile("box3.png");
-    Sprite box3(box2);
-    const float box2scale = 1.2;
-    box3.setScale(box2scale, box2scale);
-    box3.setPosition(-296, 40);
-    Texture box;
-    box.loadFromFile("boxs2.png");
-    Sprite box1(box);
-    const float boxscale = 1.2;
-    box1.setScale(boxscale, boxscale);
-    box1.setPosition(-215, 58);
-    Texture box6;
-    box6.loadFromFile("boxs2.png");
-    Sprite box7(box6);
-    const float box6scale = 1.2;
-    box7.setScale(box6scale, box6scale);
-    box7.setPosition(-356, -38);
     int x = 0, y = 0;
     Texture t;
     Texture Bg;
-    Bg.loadFromFile("background.jpg");
+    Bg.loadFromFile("background2.jpg");
     Sprite Bg1(Bg);
     const float Bgscale = 0.8;
     Bg1.setScale(Bgscale, Bgscale);
     Bg1.setPosition(0, 0);
     t.loadFromFile("yellow hair boy.png");
     Sprite YP;
-    View camera(Vector2f(0.f, 0.f), Vector2f(window.getSize().x,window.getSize().y));
+    View camera(Vector2f(0.f, 0.f), Vector2f(window.getSize().x/2,window.getSize().y/2));
     YP.setTexture(t);
     YP.setTextureRect(IntRect(x * 64, 0 * 64, 64, 64));
     YP.setPosition(window.getSize().x/ 2.f, window.getSize().y / 2.f);
@@ -77,29 +46,23 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
             RunMenuEvents(window, main, isMenuOpened, playPressed,event);
-            if (playPressed)
-                MovementSpaceShip(window, camera, YP, x, y);
 
             if (Keyboard::isKeyPressed(Keyboard::R)) {
                 game brickBreakerGame("2.Breakout C++ SFML", score_BrickBreaker);
                 brickBreakerGame.run(score_BrickBreaker);
             }
         }
+        window.clear();
         camera.setCenter(YP.getPosition());
 
-        window.clear();
         if (isMenuOpened) {
             window.draw(s_mainBG);
             main.draw(window);
         }
         if (playPressed) {
             window.setView(camera);
-            window.draw(GM1);
-            window.draw(box7);
-            window.draw(box5);
-            window.draw(box3);
-            window.draw(box1);
             window.draw(Bg1);
+            MovementSpaceShip(window, camera, YP, x, y);
             window.draw(YP);
         }
 
--- SpaceShipEvents.h
+++ SpaceShipEvents.h
@@ -8,39 +8,79 @@ void MovementSpaceShip(RenderWindow& window, View& camera,Sprite& YP,int& x,int&
 	Sprite GM1(GM);
 	const float GMscale = 0.15;
 	GM1.setScale(GMscale, GMscale);
-	GM1.setPosition(-480, -250);
+	GM1.setPosition(0, 0);
 	Texture box4;
 	box4.loadFromFile("box3.png");
 	Sprite box5(box4);
 	const float box4scale = 1.2;
 	box5.setScale(box4scale, box4scale);
-	box5.setPosition(-360, 80);
+	box5.setPosition(0, 0);
 	Texture box2;
 	box2.loadFromFile("box3.png");
 	Sprite box3(box2);
 	const float box2scale = 1.2;
 	box3.setScale(box2scale, box2scale);
-	box3.setPosition(-296, 40);
+	box3.setPosition(0, 0);
 	Texture box;
 	box.loadFromFile("boxs2.png");
 	Sprite box1(box);
 	const float boxscale = 1.2;
 	box1.setScale(boxscale, boxscale);
-	box1.setPosition(-215, 58);
+	box1.setPosition(0, 0);
 	Texture box6;
 	box6.loadFromFile("boxs2.png");
 	Sprite box7(box6);
 	const float box6scale = 1.2;
 	box7.setScale(box6scale, box6scale);
-	box7.setPosition(-356, -38);
-	int screenXBorders = 20;
-	int screenYBorders = 100;
+	box7.setPosition(0, 0);
+	int screenXRightBorders = 20;
+	int screenXLeftBorders = 20;
+	int screenYTopBorders = 100;
+	int screenYBotBorders = 200;
+
+	if (Keyboard::isKeyPressed(Keyboard::Right) && YP.getPosition().x < window.getSize().x - screenXBorders)
+	{
+		YP.move(10, 0);
+		camera.move(10, 0);
+		x++;
+		x %= 4;
+		y = 2;
+		YP.setTextureRect(IntRect(x * 64, y * 64, 64, 64));
+	}
+	else if (Keyboard::isKeyPressed(Keyboard::Left) && YP.getPosition().x > screenXBorders)
+	{
+		YP.move(-10, 0);
+		camera.move(-10, 0);
+		x++;
+		x %= 4;
+		y = 1;
+		YP.setTextureRect(IntRect(x * 64, y * 64, 64, 64));
+	}
+	else if (Keyboard::isKeyPressed(Keyboard::Up) && YP.getPosition().y > screenYTopBorders)
+	{
+		YP.move(0, -10);
+		camera.move(0, -10);
+		x++;
+		x %= 4;
+		y = 3;
+		YP.setTextureRect(IntRect(x * 64, y * 64, 64, 64));
+	}
+	else if (Keyboard::isKeyPressed(Keyboard::Down) && YP.getPosition().y < window.getSize().y - screenYBotBorders)
+	{
+		YP.move(0, 10);
+		camera.move(0, 10);
+		x++;
+		x %= 4;
+		y = 0;
+		YP.setTextureRect(IntRect(x * 64, y * 64, 64, 64));
+
+	}
 	
 	if (box1.getGlobalBounds().intersects(YP.getGlobalBounds()) || box3.getGlobalBounds().intersects(YP.getGlobalBounds()) || box5.getGlobalBounds().intersects(YP.getGlobalBounds()) || box7.getGlobalBounds().intersects(YP.getGlobalBounds()) || GM1.getGlobalBounds().intersects(YP.getGlobalBounds()))
 	{
 		if (Keyboard::isKeyPressed(Keyboard::Down))
 		{
-			YP.move(0, -0.1);
+			YP.move(0,-0.1);
 			camera.move(0, -0.1);
 		}
 		else if (Keyboard::isKeyPressed(Keyboard::Up))
@@ -59,44 +99,9 @@ void MovementSpaceShip(RenderWindow& window, View& camera,Sprite& YP,int& x,int&
 			camera.move(-0.1, 0);
 		}
 	}
-	else {
-		if (Keyboard::isKeyPressed(Keyboard::Right) && YP.getPosition().x < window.getSize().x - screenXBorders)
-	{
-		YP.move(10, 0);
-		camera.move(10, 0);
-		x++;
-		x %= 4;
-		y = 2;
-		YP.setTextureRect(IntRect(x * 64, y * 64, 64, 64));
-	}
-	else if (Keyboard::isKeyPressed(Keyboard::Left) && YP.getPosition().x > screenXBorders)
-	{
-		YP.move(-10, 0);
-		camera.move(-10, 0);
-		x++;
-		x %= 4;
-		y = 1;
-		YP.setTextureRect(IntRect(x * 64, y * 64, 64, 64));
-	}
-	else if (Keyboard::isKeyPressed(Keyboard::Up) && YP.getPosition().y > screenYBorders)
-	{
-		YP.move(0, -10);
-		camera.move(0, -10);
-		x++;
-		x %= 4;
-		y = 3;
-		YP.setTextureRect(IntRect(x * 64, y * 64, 64, 64));
-	}
-	else if (Keyboard::isKeyPressed(Keyboard::Down) && YP.getPosition().y < window.getSize().y - screenYBorders)
-	{
-		YP.move(0, 10);
-		camera.move(0, 10);
-		x++;
-		x %= 4;
-		y = 0;
-		YP.setTextureRect(IntRect(x * 64, y * 64, 64, 64));
-
-	}
-	}
-
+	window.draw(GM1);
+	window.draw(box1);
+	window.draw(box3);
+	window.draw(box5);
+	window.draw(box7);
 }
