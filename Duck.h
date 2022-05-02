6fee06e5a12cc1584fa5abdb19c25b8c0c6da7c9
MeloVakarian <kadry.momen4@gmail.com>
2022-05-02 02:03:26 +0200

    final tweaks in duck

--- Duck.h
+++ Duck.h
@@ -7,7 +7,7 @@ using namespace sf;
 struct Ship {
 public:
 	Ship();
-	void Update(RenderWindow&, float&, int&, int&, int&);
+	void Update(RenderWindow&, float&, int&, int&);
 	void Death_Check(RenderWindow&);
 	Vector2f position = { 0,0 };
 	Vector2f speed = { 0,0 };
@@ -20,7 +20,7 @@ public:
 	Texture texture5;
 	Texture texture6;
 	Texture texture7;
-
+	bool friendly = false;
 
 private:
 	bool check_in = false;
@@ -41,17 +41,18 @@ public:
 
 private:
 	int texture_index = 0;
-	Ship enemies[20];
-	Ship friendlies[20];
+	int friend_index = 0;
+	Ship enemies[40];
+	//Ship friendlies[20];
 	int directionIndex = 0;
 	int positionIndex = 0;
 	float enemyDelay = 0;
 	float delay = 0.1f;
 	float shipSpeed = 200.0f;
 	int enemiesCount = 0;
-	int friendliesCount = 0;
+	int friendliesMaxCount = 4;
 	int maxEnemies = 9;
-	int maxFriendlies = 3;
+	//int maxFriendlies = 3;
 	bool pressed = false;
 	Texture cross;
 	Texture background;
@@ -65,7 +66,7 @@ private:
 	Sprite hitEffect;
 
 	int score = 0;
-	int health = 20;
+	int health = 40;
 
 	RectangleShape crosshair;
 };
